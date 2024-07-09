3690bd298c311dfe03b3e4bc9e1e8085403f3546
Max Wilcoxson <mwilcoxson@berkeley.edu>
2024-07-09 15:02:55 -0700

    added ultra envs

--- d4rl/locomotion/__init__.py
+++ d4rl/locomotion/__init__.py
@@ -613,3 +613,71 @@ register(
         'v2_resets': True,
     }
 )
+
+register(
+    id='antmaze-ultra-diverse-v0',
+    entry_point='HIQL.d4rl_ext.locomotion.ant:make_ant_maze_env',
+    max_episode_steps=2000,
+    kwargs={
+        'deprecated': True,
+        'maze_map': maze_env.ULTRA_MAZE_TEST,
+        'reward_type':'sparse',
+        'non_zero_reset':False,
+        'dataset_url':'https://github.com/ZhengyaoJiang/d4rl/releases/download/public/Ant_maze_ultra_noisy_multistart_True_multigoal_True_sparse.hdf5',
+        'eval':True,
+        'maze_size_scaling': 4.0,
+        'ref_min_score': 0.0,
+        'ref_max_score': 1.0,
+    }
+)
+
+register(
+    id='antmaze-ultra-diverse-v0-2',
+    entry_point='HIQL.d4rl_ext.locomotion.ant:make_ant_maze_env',
+    max_episode_steps=2000,
+    kwargs={
+        'deprecated': True,
+        'maze_map': maze_env.ULTRA_MAZE_TEST_CORNER_2,
+        'reward_type':'sparse',
+        'non_zero_reset':False,
+        'dataset_url':'https://github.com/ZhengyaoJiang/d4rl/releases/download/public/Ant_maze_ultra_noisy_multistart_True_multigoal_True_sparse.hdf5',
+        'eval':True,
+        'maze_size_scaling': 4.0,
+        'ref_min_score': 0.0,
+        'ref_max_score': 1.0,
+    }
+)
+
+register(
+    id='antmaze-ultra-diverse-v0-3',
+    entry_point='HIQL.d4rl_ext.locomotion.ant:make_ant_maze_env',
+    max_episode_steps=2000,
+    kwargs={
+        'deprecated': True,
+        'maze_map': maze_env.ULTRA_MAZE_TEST_CORNER_3,
+        'reward_type':'sparse',
+        'non_zero_reset':False,
+        'dataset_url':'https://github.com/ZhengyaoJiang/d4rl/releases/download/public/Ant_maze_ultra_noisy_multistart_True_multigoal_True_sparse.hdf5',
+        'eval':True,
+        'maze_size_scaling': 4.0,
+        'ref_min_score': 0.0,
+        'ref_max_score': 1.0,
+    }
+)
+
+register(
+    id='antmaze-ultra-diverse-v0-4',
+    entry_point='HIQL.d4rl_ext.locomotion.ant:make_ant_maze_env',
+    max_episode_steps=2000,
+    kwargs={
+        'deprecated': True,
+        'maze_map': maze_env.ULTRA_MAZE_TEST_CORNER_4,
+        'reward_type':'sparse',
+        'non_zero_reset':False,
+        'dataset_url':'https://github.com/ZhengyaoJiang/d4rl/releases/download/public/Ant_maze_ultra_noisy_multistart_True_multigoal_True_sparse.hdf5',
+        'eval':True,
+        'maze_size_scaling': 4.0,
+        'ref_min_score': 0.0,
+        'ref_max_score': 1.0,
+    }
+)
